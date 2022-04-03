862927530332df6c87b17a70de5c8aa2f240128d
Yaroslav Levchenko <januariyy@gmail.com>
2022-04-03 19:19:00 +0300

    3 commit

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Hello World 2!');
 });
 
 app.listen(process.env.PORT || 3000, function() {
-  console.log('Example app listening on port 3000!');
+  console.log(`Example app listening on port ${process.env.PORT || 3000}!`);
 });
