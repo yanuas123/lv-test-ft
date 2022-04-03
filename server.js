c4972e94c9b38be2ce78bb3d3343350affb0bac7
Yaroslav Levchenko <januariyy@gmail.com>
2022-04-03 19:51:28 +0300

    first env change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello World 2!');
+  res.send('Hello World 2!' + process.env.BLA);
 });
 
 app.listen(process.env.PORT || 3000, function() {
